2df1fa96deaae03ea5c38392c96e0a6b7a6c7d0d
panreyes <panreyes@panreyes.com>
2024-07-08 23:58:38 +0200

    Corrected output filename. Translated comments

--- replace_texts_with_labels.py
+++ replace_texts_with_labels.py
@@ -1,7 +1,7 @@
 import csv
 
 def replace_in_file(source_file_path, csv_file_path, output_file_path):
-    # Cargar las traducciones desde el archivo CSV
+    # Load translations from CSV
     translations = {}
     with open(csv_file_path, encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
@@ -10,21 +10,21 @@ def replace_in_file(source_file_path, csv_file_path, output_file_path):
             translated_text = row[0]
             translations['"' + original_text + '"'] = '"' + translated_text + '"'
 
-    # Leer el contenido del archivo fuente
+    # Read source code
     with open(source_file_path, 'r', encoding='utf-8') as file:
         content = file.read()
 
-    # Reemplazar el texto según las traducciones
+    # Replace the text with the labels
     for original, translated in translations.items():
         content = content.replace(original, translated)
 
-    # Guardar el contenido modificado en un nuevo archivo
+    # Save the modified content in a new file
     with open(output_file_path, 'w', encoding='utf-8') as file:
         file.write(content)
 
-# Uso del script
-source_code_path = 'project.gbsproj'  # Cambia esto al path de tu archivo de código fuente
-csv_path = 'translation_labels.csv'                  # Cambia esto al path de tu archivo CSV
-output_path = 'project.gbsproj.out'                 # Path del archivo de salida
+# Script options
+source_code_path = 'project.gbsproj'     # Change this to the path of your GB Studio project file (.gbsproj)
+csv_path = 'translation_labels.csv'      # Change this to the path of your CSV file
+output_path = 'project.gbsproj.labeled'  # Output file path
 
 replace_in_file(source_code_path, csv_path, output_path)
